85e7a2788e3ddc82f7a6a99a48871e56b43397f4
CreativeJr <73512563+CreativeJr@users.noreply.github.com>
2022-11-28 17:38:56 +0700

    Add files via upload

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : @viral_backup,
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='@viral_backup'>Click here</a>\n○ Channel : @viral_backup\n○ Support Group : @viral_backup</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
